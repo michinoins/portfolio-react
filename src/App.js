7a30597c701f1ff8e9be82b337d1d241f11772b9
michinoins <>
2022-09-22 22:17:27 -0700

    add cursor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import AnimatedCursor from 'react-animated-cursor';
+import { ToastContainer } from 'react-toastify';
+import AOS from 'aos';
+import { useEffect } from 'react';
 
 function App() {
+  // this for animation
+  useEffect(() => {
+    AOS.init({
+      duration: 1200,
+    });
+  }, []);
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AnimatedCursor outerSize={20} />
+      {/* <ScrollToTop />
+      <AllRoutes /> */}
+      <ToastContainer />
+    </>
   );
 }
 
