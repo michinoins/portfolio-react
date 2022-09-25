b8b140381e028eae4ca2951d7727c15bb1c100c2
michinoins <>
2022-09-25 14:37:16 -0700

    add skills

--- src/components/about/Skills.jsx
+++ src/components/about/Skills.jsx
@@ -1,35 +1,80 @@
 import React from 'react';
 
-const skillsContent = [
-  { skillClass: 'p60', skillPercent: '60', skillName: 'Java' },
-  { skillClass: 'p70', skillPercent: '70', skillName: 'Kotlin' },
-  { skillClass: 'p60', skillPercent: '60', skillName: 'SpringBoot' },
-  { skillClass: 'p40', skillPercent: '40', skillName: 'HTML' },
-  { skillClass: 'p60', skillPercent: '60', skillName: 'JAVASCRIPT' },
-  { skillClass: 'p40', skillPercent: '40', skillName: 'CSS' },
-  { skillClass: 'p50', skillPercent: '50', skillName: 'REACT' },
-  { skillClass: 'p70', skillPercent: '70', skillName: 'AWS' },
-  { skillClass: 'p40', skillPercent: '40', skillName: 'Docker' },
-  { skillClass: 'p60', skillPercent: '60', skillName: 'Mysql' },
+function Skill({ source, alt, title }) {
+  return <img src={source} alt={alt} title={title} />;
+}
+const skillSet = [
+  {
+    title: 'Java',
+    alt: 'logo for Java',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg',
+  },
+  {
+    title: 'Kotlin',
+    alt: 'logo for Kotlin',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kotlin/kotlin-original-wordmark.svg',
+  },
+  {
+    title: 'SpringBoot',
+    alt: 'logo for SpringBoot',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original-wordmark.svg',
+  },
+  {
+    title: 'HTML 5',
+    alt: 'logo for HTML5',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg',
+  },
+  {
+    title: 'CSS3',
+    alt: 'logo for CSS3',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg',
+  },
+
+  {
+    title: 'Javascript',
+    alt: 'logo for Javascript',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+  },
+  {
+    title: 'React',
+    alt: 'logo for React',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg',
+  },
+
+  {
+    title: 'AWS',
+    alt: 'logo for AWS',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original-wordmark.svg',
+  },
+
+  {
+    title: 'Mysql',
+    alt: 'logo for Mysql',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg',
+  },
+
+  {
+    title: 'Docker',
+    alt: 'logo for Docker',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg',
+  },
+  {
+    title: 'Github',
+    alt: 'logo for Github',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg',
+  },
 ];
 
 const Skills = () => {
   return (
     <>
-      {skillsContent.map((val, i) => (
-        <div className='col-6 col-md-3 mb-3 mb-sm-5' key={i}>
-          <div className={`c100 ${val.skillClass}`}>
-            <span>{val.skillPercent}%</span>
-            <div className='slice'>
-              <div className='bar'></div>
-              <div className='fill'></div>
-            </div>
-          </div>
-          <h6 className='text-uppercase open-sans-font text-center mt-2 mt-sm-4'>
-            {val.skillName}
-          </h6>
+      <div className='skills'>
+        <div className='skillsGrid'>
+          {skillSet.map((val, i) => (
+            <Skill source={val.src} alt={val.alt} title={val.title} />
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 };
--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -10,7 +10,7 @@ const index = () => {
         {/* Experience & Education Starts */}
         <div className='row'>
           <div className='col-12'>
-            <h3 className='text-uppercase pb-5 mb-0 text-left text-sm-center custom-title ft-wt-600'>
+            <h3 className='mt-5 text-center'>
               Experience <span>&</span> Education
             </h3>
           </div>
@@ -30,10 +30,8 @@ const index = () => {
 
         {/* Skills Starts */}
         <div className='row'>
-          <div className='col-12'>
-            <h3 className='text-uppercase pb-4 pb-sm-5 mb-3 mb-sm-0 text-left text-sm-center custom-title ft-wt-600'>
-              My Skills
-            </h3>
+          <div className='col-12 '>
+            <h3 className='text-uppercase text-center'>My Skills</h3>
           </div>
           <Skills />
         </div>
